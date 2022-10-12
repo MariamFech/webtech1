aed953f5bc7ec351e2250a3567dae57a1f0e55f0
MariamFech <s0568845@htw-berlin.de>
2022-10-12 23:45:27 +0200

    add template and controller

--- src/main/java/com/example/demo/Webtech1DemoApplication.java
+++ src/main/java/com/example/demo/Webtech1DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Webtech1DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Webtech1DemoApplication.class, args);
 	}
 
