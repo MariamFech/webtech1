6332a4b78e92d52b6a7255e4b676e253afbd64a8
MariamFech <s0568845@htw-berlin.de>
2022-10-12 21:05:24 +0200

    commit gradle template

---
+++ src/main/java/com/example/demo/Webtech1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtech1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtech1DemoApplication.class, args);
+	}
+
+}
